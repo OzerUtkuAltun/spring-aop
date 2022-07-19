39fee7b32e451ad6c141a5aa275dae17b7e19a6d
Özer Altun <ozerutkualtun@gmail.com>
2022-07-19 09:10:15 +0300

    pointcut expressions overview on @before

--- src/main/java/com/ozerutkualtun/aop/aspects/DemoLoggingAspect.java
+++ src/main/java/com/ozerutkualtun/aop/aspects/DemoLoggingAspect.java
@@ -8,6 +8,41 @@ import org.springframework.stereotype.Component;
 @Component
 public class DemoLoggingAspect {
 
+    /*
+     pointcut expression language:
+
+     execution(modifiers-pattern? return-type-pattern? declaring-type-pattern? method-name-pattern(param-pattern) throws-pattern?)    ? = optional
+
+    "EXAMPLES: "
+
+    1- addAccount method in only AccountDao class:
+
+    @Before("execution(public void com.ozerutkualtun.aop.dao.AccountDao.addAccount())")
+
+
+    2- any addAccount method in any class:
+
+    @Before("execution(public void addAccount())")
+
+
+    3- methods with starting add in any class (using wildcards *)
+
+    @Before(execution("public void add*()"))
+
+
+    4- methods with starting processCreditCard in any class (using wildcards *)
+
+    @Before(execution("public VerificationResult processCreditCard*()"))
+
+
+    5- wildcard can be used on return type
+
+     @Before(execution("public * processCreditCard*()"))
+     @Before(execution("* processCreditCard*()")) -> modifier zaten optional
+
+   */
+
+
     @Before("execution(public void addAccount())") // içerideki expression = pointcut
     public void beforeAddAccountAdvice() {
 
